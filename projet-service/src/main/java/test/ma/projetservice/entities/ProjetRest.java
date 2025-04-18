9164d496cf011f5ebaff341fb1b2fda4fea3233c
oumaimamar <oumaimamarzouk02@gmail.com>
2025-04-18 16:58:46 +0100

    V4 new-document

--- projet-service/src/main/java/test/ma/projetservice/entities/ProjetRest.java
+++ projet-service/src/main/java/test/ma/projetservice/entities/ProjetRest.java
@@ -1,4 +1,33 @@
 package test.ma.projetservice.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class ProjetRest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String Titre;
+    private String Description;
+    private Date dateDebut;
+    private Date dateFin;
+    private String statut;
+
+
 }
